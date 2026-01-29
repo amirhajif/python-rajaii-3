1e0883a2021ad0dd8fa5470303f90e1c805ca9af
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-01-29 16:20:52 +0330

    Create session2.py

---
+++ session2.py
@@ -0,0 +1,70 @@
+# this code for session 1
+'''
+asbdmnasbdnmas dmasdAs dnm,ans
+mas,dnam.,smdjkasdn
+akdjlasmdlkajsd
+askldjkasjdadjlkdas
+'''
+
+"""
+asdkjalksdjl;asd
+aslkdjklasdlasd
+asdasdasd
+"""
+
+# print("Hello Session 1")
+
+# firstName="amirhossein"
+# lastName = "hajitabar"
+# score = 20
+# studentName2="reza"
+# studentSchoolScore = 20
+
+# print(firstName)
+# print("firstName")
+
+# username = input("please enter username: ")
+# print(username)
+
+# print("amirhossein",end=" ")
+# print("hajitabar")
+# print("from Iran")
+
+
+# name = "amir hossein"
+# print(name)
+# # SLICING
+# print(name[2])
+# print(name[4])
+# print(name[-1])
+# print(name[2:8])
+# print(name[:8])
+# print(name[8:])
+# print(name[:])
+# print(name[2:8:2])
+# print(name[::-1])
+
+# name = "amir"
+# print(name.upper())
+# city = "BABOL"
+# print(city.lower())
+# message = "how old are you?"
+# print(message[0:9].title())
+
+
+# name = "amir reza"
+# newName = name.replace("a","s")
+# print(newName)
+
+# products = "apple,xiaomi,samsung,pocco"
+# print(products.split(","))
+
+# username = " amir"
+# print(username.strip())
+
+name = "amirhossein"
+age = 24
+print(f"welcome {name} with {age} year`s old")
+print("welcome {} with {} year`s old".format(name,age))
+
+print("amirhossein\nhajitabar\tfrom iran")
