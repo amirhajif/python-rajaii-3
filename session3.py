5180bf67e14b5cf3e864258f49800877f7242550
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-02-05 16:28:20 +0330

    Create session3.py

---
+++ session3.py
@@ -0,0 +1,133 @@
+# score = 19.5
+# print(type(score))
+
+# a = 2
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a**b)
+# print(b/a)
+# print(b//a)
+# print(a%b)
+
+# r=int(input("please enter radius: "))
+# print(type(r))
+# env = 2*3*r
+# print(env)
+
+# String + *
+# firstName = "amirhossein"
+# lastName = "hajitabar"
+# fullName = firstName+" "+lastName
+# print(fullName)
+
+# name = "amir"
+# print(name*4)
+
+
+# r=float(input("please enter radius: "))
+# print(type(r))
+# env = 2*3*r
+# print(env)
+
+# money = 10_000_000
+# print(money)
+
+# firstName = "amirhossein"
+# lastName = "hajitabar"
+# age = 24
+
+# multiple assignment
+# firstName,lastName,age = "amirhossein","hajitabar",24
+
+# Collection
+'''
+1-List+ --> []
+2-Tuple ---> ()
+3-Dictionary+ ----> {}
+4-Set ----> {}
+'''
+
+# names = ["amir","reza","ahmad","mamad"]
+# scores = [20,15,18,19]
+# print(scores)
+# print(type(scores))
+# print(scores[2])
+
+
+# names = ["amir","reza","ahmad","mamad"]
+# update
+# names[3]="mohammad"
+# print(names)
+
+# append
+# names.append("jafar")
+# print(names)
+
+# insert
+# names.insert(1,"arsham")
+# print(names)
+
+# del
+# del names[0]
+# print(names)
+
+# pop
+'''
+Return Deleted Item
+names.pop(index) --> delete item in index
+names.pop() --> delete last Item
+'''
+
+# deletedName = names.pop(1)
+# print(names)
+# print(deletedName)
+
+# names.append("ahmad")
+# print(names)
+
+# Remove ---> Delete By Value
+# names.remove("ahmad")
+# print(names)
+
+
+# names = ["amir","reza","ahmad","mamad"]
+# scores = [20,15,18,19]
+# scores.sort(reverse=True)
+# print(scores)
+
+# names.sort()
+# print(names)
+
+# names.sort()
+# print(names)
+
+# sortedNames = sorted(names)
+# print(sortedNames)
+# print(names)
+
+# names.reverse()
+# print(names)
+
+# reversedNames = list(reversed(names))
+# print(reversedNames)
+# print(names)
+
+# print(len(names))
+
+# scores = [20,15,18,19]
+# print(max(scores))
+# print(min(scores))
+# print(sum(scores))
+# print(sum(scores)/len(scores))
+
+# cars = ["benz","bmw","prado"]
+# showRoomCars = cars[:]
+
+# cars.append("206")
+# showRoomCars.append("ferrari")
+
+# print(cars)
+# print(showRoomCars)
+
